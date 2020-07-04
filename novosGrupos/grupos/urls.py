f4e90ba3beffc5a6f4ae0b63227e676fe94855e0
Bruno-Felix <balvesfelix@gmail.com>
2020-07-04 04:31:19 -0300

    Criando urls para Ediar Grupos, Artistas e Comebacks

--- novosGrupos/grupos/urls.py
+++ novosGrupos/grupos/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # Import das views criadas em views.py
-from.views       import listaDeGrupos, listaDeArtista, listaDeComebacks, criarGrupo, criarArtista, criarComeback, index
+from.views       import listaDeGrupos, listaDeArtista, listaDeComebacks, criarGrupo, criarArtista, criarComeback, editarArtista, editarGrupo, editarComeback, index
 
 urlpatterns = [
     # A url /grupos vai chamar a view listaDeGrupos da views.py, que por sua vez, vai renderizar a página listaDeGrupos.html
@@ -10,5 +10,8 @@ urlpatterns = [
     path('comebacks', listaDeComebacks, name="listaDeComebacks"), 
     path('criarGrupo', criarGrupo),
     path('criarArtista', criarArtista),
-    path('criarComeback', criarComeback)
+    path('criarComeback', criarComeback),
+    path('editarGrupo/<int:id>', editarGrupo),
+    path('editarArtista/<int:id>', editarArtista),
+    path('editarComeback/<int:id>', editarComeback)
 ]
