7ace8006b665ca0f11c91792e8a80d0d5ae2c057
Bruno-Felix <balvesfelix@gmail.com>
2020-07-04 04:23:45 -0300

    Criando views Editar Grupo, Artista e Comeback

--- novosGrupos/grupos/views.py
+++ novosGrupos/grupos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 # Import das models criadas no models.py
 from .models          import Grupo, Artista, Comeback
 from .forms           import GrupoForm, ArtistaForm, ComebackForm
@@ -68,3 +68,40 @@ def criarComeback(request):
         form = ComebackForm()
 
     return render(request, "grupos/criarComeback.html", {'form':form})
+
+
+def editarGrupo(request, id=None):
+
+    grupo = get_object_or_404(Grupo, id=id)
+    form = GrupoForm(request.POST or None, instance=grupo)
+    if form.is_valid():
+        obj = form.save()
+        obj.save()
+        form = GrupoForm()
+        return redirect('listaDeGrupos')
+    
+    return render(request, "grupos/editarGrupo.html", {'form':form})
+
+def editarArtista(request, id=None):
+
+    artista = get_object_or_404(Artista, id=id)
+    form = ArtistaForm(request.POST or None, instance=artista)
+    if form.is_valid():
+        obj = form.save()
+        obj.save()
+        form = ArtistaForm()
+        return redirect('listaDeArtistas')
+    
+    return render(request, "grupos/editarArtista.html", {'form':form})
+
+def editarComeback(request, id=None):
+
+    comeback = get_object_or_404(Comeback, id=id)
+    form = ComebackForm(request.POST or None, instance=comeback)
+    if form.is_valid():
+        obj = form.save()
+        obj.save()
+        form = ComebackForm()
+        return redirect('listaDeComebacks')
+    
+    return render(request, "grupos/editarComeback.html", {'form':form})
